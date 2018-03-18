46f907147eca474b2dbb9746676b5f1567c8893a
Akash Verma <akash.verma926@gmail.com>
2018-03-18 14:46:47 +0530

    added ngIf in displayin list on weather list component

--- src/app/Components/weather/weather-list/weather-list.component.ts
+++ src/app/Components/weather/weather-list/weather-list.component.ts
@@ -9,7 +9,6 @@ import { WeatherService } from '../../../Service/weather.service';
 })
 export class WeatherListComponent implements OnInit {
   @Input() weatherrecievedbyweatherlist;
-
   constructor(private weatherService: WeatherService) { }
 
   ngOnInit() {
@@ -19,7 +18,7 @@ export class WeatherListComponent implements OnInit {
     this.weatherService.addWeather(sendToSaveInDbweather).subscribe((res) =>{
       alert("Successfully Added "+this.weatherrecievedbyweatherlist.name+" to your favourite list.");  
     }, (err) =>{
-      alert(""+this.weatherrecievedbyweatherlist.name+" is already present in your favourite list.");
+      alert(this.weatherrecievedbyweatherlist.name+" is already present in your favourite list.");
     })
   }
 }
